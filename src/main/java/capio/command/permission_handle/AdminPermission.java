9e34288de54172758380a0a555ea7c63ed8896f5
Sam Jacobson <unpluggedsam990@gmail.com>
2022-06-19 13:58:58 -0700

    Added documentation to concrete PermissionController's

--- src/main/java/capio/command/permission_handle/AdminPermission.java
+++ src/main/java/capio/command/permission_handle/AdminPermission.java
@@ -1,9 +1,13 @@
 package capio.command.permission_handle;
 
+import capio.command.bot_commands.Command;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.util.List;
 
+/**
+ * Administrative access control. Users with Admin roles have access to these {@link Command}'s.
+ */
 public class AdminPermission implements PermissionController {
     @Override
     public List getRequiredRoles(MessageReceivedEvent event) {
--- src/main/java/capio/command/permission_handle/BasicPermission.java
+++ src/main/java/capio/command/permission_handle/BasicPermission.java
@@ -1,10 +1,14 @@
 package capio.command.permission_handle;
 
+import capio.command.bot_commands.Command;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.util.List;
 
+/**
+ * Basic permission access. Everyone (besides muted Users) should have access to {@link Command}'s with these rights.
+ */
 public class BasicPermission implements PermissionController {
     @Override
     public List<Role> getRequiredRoles(MessageReceivedEvent event) {
