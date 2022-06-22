2eaa76626a03eebdaba41caec5b41819aeae0d3f
Sam Jacobson <unpluggedsam990@gmail.com>
2022-06-21 20:07:53 -0700

    Added a method to check if the User meets the guild requirements
    This method will be filled out later

--- src/main/java/capio/minecraft/hypixel/RequiredStatistics.java
+++ src/main/java/capio/minecraft/hypixel/RequiredStatistics.java
@@ -1,4 +1,10 @@
 package capio.minecraft.hypixel;
 
+import capio.minecraft.users.Player;
+
 public class RequiredStatistics {
+    
+    public boolean doesUserMeetRequirements(Player player) {
+        return false;
+    }
 }
