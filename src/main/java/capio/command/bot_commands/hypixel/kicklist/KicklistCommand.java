21ef9a6b2c95826c5b6dd91e764c0556bcc77069
Sam Jacobson <unpluggedsam990@gmail.com>
2022-06-21 20:04:48 -0700

    Added skeleton for in game bot commands and the kicklist command

---
+++ src/main/java/capio/command/bot_commands/hypixel/kicklist/KicklistCommand.java
@@ -0,0 +1,21 @@
+package capio.command.bot_commands.hypixel.kicklist;
+
+import capio.command.bot_commands.Command;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class KicklistCommand implements Command {
+    @Override
+    public void execute(MessageReceivedEvent event, String[] args) {
+        
+    }
+
+    @Override
+    public String getCommandName() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+}
---
+++ src/main/java/capio/command/bot_commands/in_game_commands/guild_managment/GuildInviteCommand.java
@@ -0,0 +1,21 @@
+package capio.command.bot_commands.in_game_commands.guild_managment;
+
+import capio.command.bot_commands.Command;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class GuildInviteCommand implements Command {
+    @Override
+    public void execute(MessageReceivedEvent event, String[] args) {
+
+    }
+
+    @Override
+    public String getCommandName() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+}
