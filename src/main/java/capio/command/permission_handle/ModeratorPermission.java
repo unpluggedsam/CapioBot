65c80c3f85b606437f3b24f38d90f3ef866a7ac1
Sam Jacobson <unpluggedsam990@gmail.com>
2022-06-21 17:22:53 -0700

    Added ModeratorPermission

---
+++ src/main/java/capio/command/permission_handle/ModeratorPermission.java
@@ -0,0 +1,13 @@
+package capio.command.permission_handle;
+
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.util.List;
+
+public class ModeratorPermission implements PermissionController {
+    @Override
+    public List<Role> getRequiredRoles(MessageReceivedEvent event) {
+        return List.of(RoleFactory.createRole(event.getGuild(), 988961939965243423L, "Moderator"));
+    }
+}
