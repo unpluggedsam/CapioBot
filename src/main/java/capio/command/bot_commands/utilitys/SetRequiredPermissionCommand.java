0fa41005b5d7c8c0cec0b80a3b1f9c00a8f8d681
Sam Jacobson <96966386+unpluggedsam@users.noreply.github.com>
2022-07-04 03:22:03 -0700

    Made the check against the permission enum case insensitive

--- src/main/java/capio/command/bot_commands/utilitys/SetRequiredPermissionCommand.java
+++ src/main/java/capio/command/bot_commands/utilitys/SetRequiredPermissionCommand.java
@@ -11,7 +11,8 @@ import java.util.List;
 public class SetRequiredPermissionCommand implements Command {
     @Override
     public void execute(MessageReceivedEvent event, String[] args, GuildCommandsHandler guildCommandsHandler) {
-        guildCommandsHandler.getGuildPermissionController(event.getGuild()).get(PermissionEnum.valueOf(args[1])).addRequiredRole(event.getMessage().getMentions().getRoles().get(0));
+        guildCommandsHandler.getGuildPermissionController(event.getGuild()).get(Arrays.stream(PermissionEnum.values())
+                .filter(e -> e.name().equalsIgnoreCase(args[1])).findAny().orElse(null)).addRequiredRole(event.getMessage().getMentions().getRoles().get(0));
     }
 
     @Override
