d4393aa7a01ec4d7b22f4dbcc4dd22aa2f71c07c
Sam Jacobson <96966386+unpluggedsam@users.noreply.github.com>
2022-07-07 22:39:40 -0700

    Create HypixelAPIKey.java

---
+++ src/main/java/capio/minecraft/hypixel/HypixelAPIKey.java
@@ -0,0 +1,13 @@
+package capio.minecraft.hypixel;
+import capio.config.ConfigReader;
+import net.hypixel.api.HypixelAPI;
+import java.util.UUID;
+public class HypixelAPIKey {
+    static HypixelAPI api;
+    private HypixelAPIKey() {
+    }
+    public static HypixelAPI getHypixelAPIKey() {
+        if(HypixelAPIKey.api == null) HypixelAPIKey.api = new HypixelAPI(UUID.fromString(ConfigReader.getHypixelAPIKey()));
+        return HypixelAPIKey.api;
+    }
+}
